21db16c2cf6f20f56ba2ced76a89e66233279896
hepanchen <hepanchen@meizu.com>
2017-09-20 19:16:08 +0800

    增加遗漏的头文件

---
+++ conn/el/el.h
@@ -0,0 +1,39 @@
+#ifndef C_REDIS_CLI_EL_EL_H_
+#define C_REDIS_CLI_EL_EL_H_
+
+
+typedef void(*FuncPtr)(void* data); //定义函数指针类型  
+
+const int maxEventsNum = 1024;
+
+
+void printerrno();
+//interface
+class EventHandler {
+public:
+    virtual int rFunc() = 0;
+    virtual int wFunc() = 0;
+    virtual int eFunc() = 0;
+};
+
+struct IOEvent {
+    int listened;
+    int events;
+    EventHandler* eventHandler;
+};
+
+class El {
+    int epfd_;
+    struct IOEvent listened_events_[maxEventsNum];
+//    std::list<struct IOEvent> fired_events_;
+
+public:
+    El();
+    int AddIOReadEvent(int fd, struct EventHandler* eventHandler);
+    int AddIOWriteEvent(int fd, struct EventHandler* eventHandler);
+    int DelIOWriteEvent(int fd);
+    int AddIOErrorEvent(int fd, struct EventHandler* eventHandler);
+    int MainLoop();
+};
+
+#endif //C_REDIS_CLI_EL_EL_H_
